57b6a69f91265c6364706e541fb119b00a7c68a9
rarrunategu1 <parker.rose@ymail.com>
2019-03-15 19:09:32 -0400

    Program writes a line and waits for user input

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -10,6 +10,8 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello, world!");
+            Console.ReadLine();
         }
     }
 }
