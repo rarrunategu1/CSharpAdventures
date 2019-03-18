2a4a4912505d4ee743aedf5155cc5448dd15f6eb
rarrunategu1 <parker.rose@ymail.com>
2019-03-17 20:13:06 -0400

    Program shows how can you access a variable in any method in that particular class

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -4,13 +4,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp
+namespace VariableScope
 {
     class Program
-    {
+    {   
+        //this field on the program class can be access from all the methods of this class
+        private static string helloClass = "Hello, class";
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, world!");
+            //this variable cannot be accessed outside of this method
+            string helloLocal = "Hello, local variable!";
+            Console.WriteLine(helloLocal);
+            Console.WriteLine(Program.helloClass);
+            AndAction();
+        }
+
+        static void AndAction()
+        {
+            Console.WriteLine("A message from AndAction: " + Program.helloClass);
             Console.ReadLine();
         }
     }
